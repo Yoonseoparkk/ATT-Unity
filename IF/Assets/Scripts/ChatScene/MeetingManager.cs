e38335fd94d559f9ee0182047b17a833cabd75d5
Yoonseoparkk <patpark97@gmail.com>
2024-09-08 16:31:28 +0900

    feat: [박윤서] 약속 날짜 퀘스트 완료시 화면에 표시 [ATTU-11]

--- IF/Assets/Scripts/ChatScene/MeetingManager.cs
+++ IF/Assets/Scripts/ChatScene/MeetingManager.cs
@@ -21,45 +21,4 @@ public class MeetingManager : MonoBehaviour
             letsMeet = false;
         }
     }
-
-
-    private Dictionary<string, string> keywordMeetingPlace = new Dictionary<string, string>()
-    {
-        // 약속 장소 결정 키워드
-        {"에서 보는 거 어때요?", "광화문"},
-        {"에서 만나요", "광화문"},
-    };
-    
-    private Dictionary<string, string> keywordMeetingDay = new Dictionary<string, string>()
-    {
-        // 약속 날짜 결정 키워드
-        {"요일이 편해요", "토요일"},
-        {"요일 좋", "토요일"},
-        {"요일 어때", "토요일"},
-        {"요일에 봐요", "토요일"},
-    };
-
-    public void UpdateMeetingPlace(string message)
-    {
-        foreach (var keyword in keywordMeetingPlace.Keys)
-        {
-            if (message.Contains(keyword))
-            {
-                meetingPlace.text = keywordMeetingPlace[keyword];
-                break; // 첫 번째 일치하는 키워드에 대해서만 약속 장소 반영
-            }
-        }
-    }
-    
-    public void UpdateMeetingDay(string message)
-    {
-        foreach (var keyword in keywordMeetingDay.Keys)
-        {
-            if (message.Contains(keyword))
-            {
-                meetingDay.text = keywordMeetingDay[keyword];
-                break; // 첫 번째 일치하는 키워드에 대해서만 약속 날짜 반영
-            }
-        }
-    }
 }
--- IF/Assets/Scripts/ChatScene/TextFromVue.cs
+++ IF/Assets/Scripts/ChatScene/TextFromVue.cs
@@ -8,11 +8,15 @@ using System;
 public class TextFromVue : MonoBehaviour
 {
     ChatManager ChatManager;
+    MeetingManager MeetingManager;
+
     string chatMessage;
+    string meetDate;
 
     public void Start()
     {
         ChatManager = GetComponent<ChatManager>();
+        MeetingManager = GetComponent<MeetingManager>();
     }
 
     // JavaScript에서 호출할 수 있는 Unity 메서드
@@ -31,5 +35,30 @@ public class TextFromVue : MonoBehaviour
         }
 
         chatMessage = "";
+    }
+
+    public void DateEvent(string date)
+    {
+        Debug.Log("Message received from JavaScript: " + date);
+
+        if (date == "")
+        {
+            meetDate = "미정";   // 아직 약속 날짜가 정해지지 않고 데이터를 받아올 경우
+        }
+        else
+        {
+            meetDate = date;    
+        }
+
+        try
+        {
+            MeetingManager.meetingDay.text = meetDate;
+        }
+        catch (NullReferenceException ex)
+        {
+            print(ex);
+        }
+
+        meetDate = "";
     }
 }
