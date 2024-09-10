3ac99a16b47d4bf9f41f479f12d4408e8a727c12
Yoonseoparkk <patpark97@gmail.com>
2024-09-10 10:41:14 +0900

    feat: [박윤서] 식사메뉴 퀘스트 완료 시 화면에 완료 여부 표시 [ATTU-15]

--- IF/Assets/Scripts/CafeScene/LikabilityManager2.cs
+++ IF/Assets/Scripts/CafeScene/LikabilityManager2.cs
@@ -7,6 +7,13 @@ public class LikabilityManager2 : MonoBehaviour
     public float minLikability = 0f;
     public float maxLikability = 100f;
 
+    private AudioManager AM;
+
+    void Start()
+    {
+        AM = GetComponent<AudioManager>();
+    }
+
     private Dictionary<string, float> keywordLikability = new Dictionary<string, float>()
     {
         // 호감도 증가 키워드
@@ -15,12 +22,13 @@ public class LikabilityManager2 : MonoBehaviour
         {"멋져", 5f},
         {"이뻐", 5f},
         {"예뻐", 5f},
+        {"기대", 5f},
         {"따뜻", 5f},
         {"믿어", 5f},
         {"행복", 5f},
         {"고마워", 5f},
         {"감사", 5f},
-        {"좋아", 5f},
+        {"좋", 5f},
         {"신나", 5f},
         {"같이", 5f},
         {"가자", 5f},
@@ -34,6 +42,9 @@ public class LikabilityManager2 : MonoBehaviour
         {"덕분", 5f},
         {"웃음", 5f},
         {"웃겨", 5f},
+        {"ㅎㅎ", 5f},
+        {"~", 5f},
+        {"!", 5f},
 
         // 호감도 감소 키워드
         {"최악", -5f},
@@ -76,10 +87,25 @@ public class LikabilityManager2 : MonoBehaviour
         {
             if (message.Contains(keyword))
             {
+                if (keywordLikability[keyword] < 0)
+                {
+                    AM.audio.clip = AM.ac[2];
+                    AM.audio.Play();    // 호감도 감소 사운드
+                }
+                else
+                {
+                    AM.audio.clip = AM.ac[1];
+                    AM.audio.Play();    // 호감도 상승 사운드
+                }
                 currentLikability += keywordLikability[keyword];
                 currentLikability = Mathf.Clamp(currentLikability, minLikability, maxLikability);
                 break; // 첫 번째 일치하는 키워드에 대해서만 호감도 변경
             }
+        }
+
+        if (currentLikability >= 70f)   // 식사 메뉴 정하기 퀘스트 활성화
+        {
+            GetComponent<DiningManager>().letsEat = true;
         }
     }
 
